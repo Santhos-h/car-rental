8d5b6a27f81a2125a9118d78d45add7675ead904
Santhosh <163711416+Santhos-h@users.noreply.github.com>
2025-04-02 15:58:05 +0530

    Update Nav.jsx

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -10,7 +10,7 @@ const Nav = () => {
                 <img src="https://mllafnlfvfl1.i.optimole.com/w:auto/h:auto/q:mauto/ig:avif/https://carspot.e-plugins.com/wp-content/uploads/2024/03/Frame-143-1.png" alt="" />
             </div>
             <ul>
-                <li><Link className='nl' to={'/'}>Home</Link></li>
+                <li><Link className='nl' to={'/car-rental'}>Home</Link></li>
                 <li><Link className='nl' to={'/listing'}>Listing</Link></li>
                 <li><Link className='nl' to={'/account'}>My Account</Link></li>
                 <li><Link className='nl' to={'/register'}>Registration</Link></li>
